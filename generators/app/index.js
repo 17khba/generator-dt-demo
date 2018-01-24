2e6437856595c520922e6c4cb9e5968a12c80b93
17khba <17khbaboy@gmail.com>
2018-01-24 11:42:53 +0800

    版本更新

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,16 +1,70 @@
 var Generator = require("yeoman-generator");
 
 module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.argument("project", {
+      type: String,
+      required: false
+    });
+
+    this.name = path.basename(process.cwd());
+  }
   rompting() {
     return this.prompt([
       {
         type: "input",
-        name: "name",
+        name: "project",
         message: "Your project name",
-        default: this.appname // Default to current folder name
+        default: this.name
+      },
+      {
+        type: "input",
+        name: "user",
+        message: "Your name",
+        default: "yourname"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Your email",
+        default: "youremail"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "description",
+        default: "A project template"
       }
     ]).then(answers => {
-      this.log("app name", answers.name);
+      this.project = answers.project || this.options.project;
+      this.user = answers.user;
+      this.email = answers.email;
+      this.description = answers.description;
+
+      this.log("project", this.project);
+      this.log("user", this.user);
+      this.log("email", this.email);
+      this.log("description", this.description);
     });
+  }
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath("./"),
+      this.destinationPath("./"),
+      {
+        project: this.project,
+        user: this.user,
+        email: this.email,
+        description: this.description
+      },
+      {},
+      {
+        globOptions: {
+          dot: true
+        }
+      }
+    );
   }
 };
